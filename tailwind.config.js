052cc80ff2e219f6bac68969b5b89a69e1c3925f
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2024-07-10 16:03:02 -0600

    feat: new proyect VARUNA

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // o 'media' o 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
