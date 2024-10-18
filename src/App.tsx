98d3a8a32b013453a9982ca8f6f80713d3e4a692
Mafer-Rodriguez <utm22030609@utma.edu.mx>
2024-10-18 02:15:17 -0600

    Feat: add new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 //import Login from './Login.tsx';
 //import Registrarse from './registrarse.tsx';
 //import Historial from './Historial.tsx';
+//import Blockchain from './blockchain.tsx';
 //import Index from './index.tsx'
 //import Barra from './barra.tsx'
-import Blockchain from './blockchain.tsx'
+//import Blockchain from './blockchain.tsx'
+import PerfilesCriminales from './perfilCriminales.tsx'
 
 function App() {
   return (
     <div className='App'>
-      <Blockchain/>
+      <PerfilesCriminales/>
     </div>
   )
 }
